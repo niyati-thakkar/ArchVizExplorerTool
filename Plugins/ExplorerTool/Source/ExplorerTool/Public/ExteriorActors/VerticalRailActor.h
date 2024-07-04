1c62e5c5d07e5c89608b72f8eedfaf6e28c8890e
Niyati Thakkar <ce2020_107075@Ldceahm.gujgov.edu.in>
2024-07-04 12:11:56 +0530

    added meshes and materials

--- Plugins/ExplorerTool/Source/ExplorerTool/Public/ExteriorActors/VerticalRailActor.h
+++ Plugins/ExplorerTool/Source/ExplorerTool/Public/ExteriorActors/VerticalRailActor.h
@@ -5,7 +5,6 @@
 #include "CoreMinimal.h"
 #include "GameFramework/Actor.h"
 #include "ProceduralMeshComponent.h"
-#include "FenceDataAsset.h"
 #include "VerticalRailActor.generated.h"
 
 USTRUCT(BlueprintType)
@@ -28,6 +27,39 @@ struct FFenceProperties
 };
 
 
+USTRUCT(BlueprintType)
+struct FFenceClassPair
+{
+    GENERATED_BODY()
+
+public:
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Fence Data")
+    TSubclassOf<AActor> ActorClass;
+
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Fence Data")
+    UStaticMesh* MeshClass;
+};
+UENUM(BlueprintType)
+enum class EFenceType : uint8
+{
+    RoundedOverTopCapital UMETA(DisplayName = "Rounded Over Top Capital"),
+    ACornCapital UMETA(DisplayName = "ACorn Capital"),
+    RoundTurnedCapital UMETA(DisplayName = "Round Turned Capital"),
+    WindsorTurnedCapital UMETA(DisplayName = "Windsor Turned Capital"),
+    GothicStarCapital UMETA(DisplayName = "Gothic Star Capital"),
+    PyramidTop UMETA(DisplayName = "Pyramid Top"),
+    Random UMETA(DisplayName = "Random")
+};
+
+UENUM(BlueprintType)
+enum class EMaterialType : uint8
+{
+    Oak_Wood UMETA(DisplayName = "Oak Wood"),
+    Pine_Wood UMETA(DisplayName = "Pine Wood"),
+    Walnut_Wood UMETA(DisplayName = "Walnut Wood"),
+    Walnut_Floor_Wood UMETA(DisplayName = "Walnut Floor Wood"),
+    Random UMETA(DisplayName = "Random")
+};
 
 UCLASS()
 class EXPLORERTOOL_API AVerticalRailActor : public AActor
@@ -72,10 +104,6 @@ public:
 
     UPROPERTY(EditAnywhere, BlueprintReadWrite)
     FFenceProperties FenceProperty;
-
-    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Fence Data")
-    UFenceDataAsset* FenceData;
-
 
     UFUNCTION(BlueprintCallable, Category = Defaults)
     void RoundTurnedCapital();
